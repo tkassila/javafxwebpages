5ec98a2c96afd8a73bbba0d9c1794cbebdcb3cd1
Tuomas Kassila <tuomas.kassila@gmail.com>
2021-12-03 12:35:32 +0200

    Removed icu jar from start script files

--- run.bat
+++ run.bat
@@ -2,4 +2,6 @@
 call \java\jdk11fxcp.bat
 set rundir=%~dp0
 echo  rundir=%curdir%
-java -cp %rundir%javafxwebpages-1.0-SNAPSHOT.jar;%rundir%lib\gson-2.8.9.jar;%rundir%lib\icu4j-70.1.jar;%CLASSPATH% com.metait.javafxwebpages.WebPagesApplication
+rem icu is not needed any more:
+rem %rundir%lib\icu4j-70.1.jar;
+java -cp %rundir%javafxwebpages-1.0-SNAPSHOT.jar;%rundir%lib\gson-2.8.9.jar;%CLASSPATH% com.metait.javafxwebpages.WebPagesApplication
--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 rundir=$(pwd)
 export rundir
 #echo rundir=$rundir
-java -cp $rundir/javafxwebpages-1.0-SNAPSHOT.jar:$rundir/lib/gson-2.8.9.jar:$rundir/lib/icu4j-70.1.jar:$CLASSPATH com.metait.javafxwebpages.WebPagesApplication
+# icu is not needed any more:
+# $rundir/lib/icu4j-70.1.jar:
+java -cp $rundir/javafxwebpages-1.0-SNAPSHOT.jar:$rundir/lib/gson-2.8.9.jar:$CLASSPATH com.metait.javafxwebpages.WebPagesApplication
